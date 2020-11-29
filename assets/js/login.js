4b025e8c27720aac14dd92324a35499591e1fb9c
zhuyulin <1282537742@qq.com>
2020-11-29 19:40:53 +0800

    登录/注册页面完成

---
+++ assets/js/login.js
@@ -0,0 +1,54 @@
+$(function(){
+    $('#link-lg').on('click',function(){
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    $('#link-reg').on('click',function(){
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+    //表单验证
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=repassword]').val();
+            if(value !== pwd) {
+                return '两次密码输入不一致';
+            }
+        }
+    })
+    // 监听注册提交事件
+    $('#form-reg').on('submit',function(e){
+        e.preventDefault();
+        var data = {
+            username: $('#form-reg [name=username]').val(),
+            password: $('#form-reg [name=password]').val(),
+        };
+        $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
+            if(res.status !== 0) {
+                layer.msg(res.message);
+            }
+            layer.msg('注册成功');
+            //模拟点击事件
+            $('#link_login').click();
+        })
+    })
+    //监听登录提交事件
+    $('#form-lg').on('submit',function(e){
+        e.preventDefault();
+        var data = {
+            username: $('#form-lg [name=username]').val(),
+            password: $('#form-lg [name=password]').val(),
+        };
+        $.post('http://ajax.frontend.itheima.net/api/login',data,function(res){
+            if(res.status !== 0) {
+                layer.msg(res.message);
+            }
+            layer.msg('登录成功');
+            localStorage.setItem('token',res.token);
+            window.location = 'index.html';
+        })
+    })
+})
